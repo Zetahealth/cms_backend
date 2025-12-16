db0423f0dd08522e5ec48acd66d74c4a9c941b5a
thirukumaran <thiru@saastrail.com>
2025-12-16 09:23:53 +0530

    after chnage the code password

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,28 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-User.find_or_create_by!(email: "admin@yopmail.com") do |user|
-    user.password = "admin@1234"
-    user.password_confirmation = "admin@1234"
-    user.role = "1"
-    user.name = "Admin"
+# User.find_or_create_by!(email: "admin@yopmail.com") do |user|
+#     user.password = "admin@1234"
+#     user.password_confirmation = "Cmf@1234"
+#     user.role = "1"
+#     user.name = "Admin"
+#     user.permission = '2'
+# end
+
+user = User.find_by(email: "admin@yopmail.com")
+
+if user
+  user.password = "Cmf@1234"
+  user.password_confirmation = "Cmf@1234"
+  user.save!(validate: false)  # ← IMPORTANT
+else
+  User.create!(
+    email: "admin@yopmail.com",
+    password: "Cmf@1234",
+    password_confirmation: "Cmf@1234",
+    role: "1",
+    name: "Admin",
+    permission: "2"
+  )
 end
+
